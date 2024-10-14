924bf08b0a1f597a44fe4b80141850cd6d09c4ef
cTangonan123 <ctangonan@csumb.edu>
2024-10-14 16:27:52 -0700

    added tpa4_chat_client.py and tpa4_chat_server.py all looks ok sof far, TODO: make network topology in excalidraw -

---
+++ tpa4_chat_client.py
@@ -0,0 +1,84 @@
+"""Chat client for CST311 Programming Assignment 4"""
+__author__ = "[Group 4]"
+__credits__ = [
+    "Chris Tangonan",
+    "Edward Torres",
+    "Victoria Ramirez",
+    "Guillermo Zendejas"
+]
+
+# Import statements
+import socket as s
+import threading
+
+# Configure logging
+import logging
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+# Set global variables
+server_name = '10.0.0.1'  # should we set to have node h1 running server code correct?
+server_port = 12000
+
+
+def incoming_message_handler(client_socket):
+    while True:
+        try:
+            incoming_message = client_socket.recv(1024)
+            if incoming_message:
+                print(incoming_message.decode())
+            else:
+                break
+        except:
+            break
+
+
+def main():
+    # Create socket
+    client_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    t = threading.Thread(target=incoming_message_handler, args=(client_socket,))
+    t.start()
+    try:
+        # Establish TCP connection
+        client_socket.connect((server_name, server_port))
+    except Exception as e:
+        log.exception(e)
+        log.error("***Advice:***")
+        if isinstance(e, s.gaierror):
+            log.error("\tCheck that server_name and server_port are set correctly.")
+        elif isinstance(e, ConnectionRefusedError):
+            log.error("\tCheck that server is running and the address is correct")
+        else:
+            log.error("\tNo specific advice, please contact teaching staff and include text of error and code.")
+        exit(8)
+
+    # Get input from user
+    user_input = input('Welcome to the chat! Please Enter in your username:\n')
+
+    try:
+        # keep the connection open as long as the message is not 'bye'
+        while user_input != 'bye':
+            # Set data across socket to server
+            #  Note: encode() converts the string to UTF-8 for transmission
+            client_socket.send(user_input.encode())
+
+            # Get input from user
+            user_input = input()
+
+    finally:
+        # set user_input to 'bye' so server recieves final message
+        user_input = 'bye'
+        # Set data across socket to server
+        #  Note: encode() converts the string to UTF-8 for transmission
+        client_socket.send(user_input.encode())
+        # Close socket prior to exit
+        client_socket.close()
+        t.join()
+
+
+# This helps shield code from running when we import the module
+if __name__ == "__main__":
+    main()
---
+++ tpa4_chat_server.py
@@ -0,0 +1,154 @@
+#!env python
+
+"""Chat server for CST311 Programming Assignment 4"""
+__author__ = "[Group 4]"
+__credits__ = [
+    "Chris Tangonan",
+    "Edward Torres",
+    "Victoria Ramirez",
+    "Guillermo Zendejas"
+]
+
+import socket as s
+import time
+import threading
+
+# Configure logging
+import logging
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+server_port = 12000
+thread_list = []
+connections = []
+addresses = []
+pending_msgs = []
+client_count = 0
+client_count_max = 3
+is_all_clients_exit = False
+
+
+def connection_handler(connection_socket, address):
+    global connections, addresses, client_count, pending_msgs, is_all_clients_exit
+    receiver1 = None
+    receiver2 = None
+
+    # assign initial query to connections user_name
+    query = connection_socket.recv(1024).decode()
+    user_name = query
+    user_id = len(connections) - 1
+
+    # get all pending messages sent to current client connection
+    for msg in pending_msgs:
+        connection_socket.send(msg.encode())
+        time.sleep(.1)
+
+    # Initial message to other clients
+    query = connection_socket.recv(1024).decode()
+    while query != "bye":
+
+        # condition of only one client in the server, append all msgs to pending_msgs
+        if (client_count == 1):
+            pending_msgs.append(user_name + ": " + query)
+
+        # condition of only two client in the server: send 1, append for other
+        elif (client_count < client_count_max):
+            if (address[0] == addresses[0][0]):
+                receiver1 = connections[1]
+            else:
+                receiver1 = connections[0]
+
+            # send currently connected receiver the message
+            receiver1.send(str(user_name + ": " + query).encode())
+            # append mesage to pending_msgs for other receiver to receive at run time
+            pending_msgs.append(user_name + ": " + query)
+
+        # condition of all three clients connnected: no appending, just sendinng
+        else:
+            if (address[0] == addresses[0][0]):
+                receiver1 = connections[1]
+                receiver2 = connections[2]
+            elif (address[0] == addresses[1][0]):
+                receiver1 = connections[0]
+                receiver2 = connections[2]
+            else:
+                receiver1 = connections[0]
+                receiver2 = connections[1]
+
+            # send message to all currently addressable receivers
+            receiver1.send(str(user_name + ": " + query).encode())
+            receiver2.send(str(user_name + ": " + query).encode())
+            pending_msgs.append(user_name + ": " + query)
+
+        # this blocks, if no message has been received
+        query = connection_socket.recv(1024).decode()
+
+    # connection close out after client says bye
+    # decrement client_count, take out address, and connection_socket from associated arrays
+    addresses.remove(address)
+    connections.remove(connection_socket)
+    client_count -= 1
+
+    for receiver in connections:
+        receiver.send(str(user_name + ": " + query).encode())
+
+    if client_count == 0:
+        is_all_clients_exit = True
+
+    # Close client socket
+    connection_socket.close()
+
+
+def main():
+    global connections, addresses, client_count, exitServer
+    # Create a TCP socket
+    # Notice the use of SOCK_STREAM for TCP packets
+    server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+
+    # Assign port number to socket, and bind to chosen port
+    server_socket.bind(('', server_port))
+
+    # Configure how many requests can be queued on the server at once
+    server_socket.listen(client_count_max)
+
+    # Alert user we are now online
+    log.info("The server is ready to receive on port " + str(server_port))
+
+    # Surround with a try-finally to ensure we clean up the socket after we're done
+    try:
+        # Enter forever loop to listen for requests
+        for i in range(client_count_max):
+            # When a client connects, create a new socket and record their address
+
+            connection_socket, address = server_socket.accept()
+            client_count += 1
+            connections.append(connection_socket)
+            addresses.append(address)
+
+            log.info("Connected to client at " + str(address) + "\nconnection_socket: " + str(connection_socket))
+
+            # TODO: Determine if address has already visited server
+
+            # Pass the new socket and address off to a connection handler function
+            # connection_handler(connection_socket, address)
+
+            # implementation of threading
+            t = threading.Thread(target=connection_handler, args=(connection_socket, address))
+
+            # starts thread, which initiates connection_handler function for the current client connected.
+            t.start()
+
+            # append thread to thread list
+            thread_list.append(t)
+    finally:
+        for t in thread_list:
+            t.join()
+        server_socket.close()
+
+        # joins the existing threads in thread_list
+
+
+if __name__ == "__main__":
+    main()
